1881c4e0ea9011c5ec9d0f65503c45f588486847
Jairo Lozano <jairo25492@gmail.com>
2024-09-19 19:56:08 -0600

    Add placeholder components for the account summaries

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
 
+function ListItem({text}){
+  return <div className="list-item">{text}</div>
+}
+
 function App() {
+
+  const accountSummaries = Array.from({length:5}, (_, index) => `Item ${index + 1}`)
+
   return (
-    <>
-      Hola vite
-    </>
+    <div className="app-container">
+      {/* Section 1: Horizontal list of components */}
+      <section className="horizontal-list">
+        {
+          accountSummaries.map((item, index) => (
+            <ListItem key={index} text={item}/>
+          ))
+        }
+      </section>
+    </div>
   )
 }
 
