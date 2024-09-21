dfff9eb586c47aed48649be03df25cb47b430b49
Jairo Lozano <jairo25492@gmail.com>
2024-09-21 13:05:01 -0600

    Lists transactions

--- src/pages/TransactionsPage/TransactionsPage.jsx
+++ src/pages/TransactionsPage/TransactionsPage.jsx
@@ -1,6 +1,7 @@
 import AddForm from "../../components/transactions/AddForm/AddForm";
 import TransactionsByCategorySummaryList from "../../components/transactions/TransactionsByCategorySummaryList/TransactionsByCategorySummaryList";
 import "./TransactionsPage.css";
+import TransactionsList from "../../components/transactions/TransactionsList/TransactionsList";
 
 function TransactionsPage() {
   return (
@@ -8,6 +9,7 @@ function TransactionsPage() {
       {/* Section 1: Horizontal list of components */}
       <TransactionsByCategorySummaryList />
       <AddForm />
+      <TransactionsList />
     </div>
   );
 }
