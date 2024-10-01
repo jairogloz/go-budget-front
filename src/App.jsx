fa928538d4999bb4c89183849c07ea38003e641b
Jairo Lozano <jairo25492@gmail.com>
2024-10-01 09:16:59 -0600

    Makes session available through context

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { supabase } from "./lib/helper/supabaseClient";
 import TransactionsPage from "./pages/TransactionsPage/TransactionsPage";
+import { useSession } from "./context/SessionContext";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const sessionCtx = useSession();
+
   const login = () => {
     supabase.auth.signInWithOAuth({
       provider: "google",
@@ -12,39 +14,12 @@ function App() {
   };
   const logout = () => {
     supabase.auth.signOut();
-    setUser(null);
+    console.log("session:", sessionCtx);
   };
 
-  // Use effect to handle authentication changes
-  useEffect(() => {
-    // Check if there is an active session when the app loads
-    const getSession = async () => {
-      const { data, error } = await supabase.auth.getSession();
-      if (data?.session) {
-        setUser(data.session.user);
-      }
-    };
-
-    getSession();
-
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      (event, session) => {
-        console.log("Auth event:", event);
-        if (event === "SIGNED_IN") {
-          setUser(session?.user);
-        }
-        if (event === "SIGNED_OUT") {
-          setUser(null);
-        }
-      }
-    );
-    return () => {
-      authListener?.subscription.unsubscribe();
-    };
-  });
   return (
     <div className="app-container">
-      {user ? (
+      {sessionCtx.session ? (
         <>
           <button onClick={logout}>Logout</button>
           <TransactionsPage />
